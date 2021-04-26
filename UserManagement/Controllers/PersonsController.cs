a04619e252fa7473aac949c4f978dfcd12770d98
Abror-1221 <muliausman12@gmail.com>
2021-04-26 16:52:24 +0700

    Add project files.

---
+++ UserManagement/Controllers/PersonsController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserManagement.Models;
+using UserManagement.Repository;
+using UserManagement.Repository.Interface;
+
+namespace UserManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonsController : ControllerBase
+    {
+        private readonly PersonRepository personRepository;
+        public PersonsController(PersonRepository person)
+        {
+            this.personRepository = person;
+        }
+
+        [HttpPost]
+        public ActionResult Post(Person person)
+        {
+            personRepository.insert(person);
+            return Ok();
+        }
+
+        ////[HttpGet]
+        //public ActionResult Get()
+        //{
+        //    IEnumerable<Person> persons = personRepository.Get();
+        //    return Ok(persons);
+        //}
+        [HttpGet]
+        public ActionResult Get(string NIK)
+        {
+            var person = personRepository.Get(NIK);
+            return Ok(person);
+        }
+
+        //COBA2 DELETE SYNTAX
+        [HttpDelete]
+        public ActionResult GetActionResult(string NIK)
+        {
+            personRepository.delete(NIK);
+            return Ok();
+        }
+
+        [HttpPut]
+        public ActionResult Update(Person person)
+        {
+            personRepository.update(person);
+            return Ok();
+        }
+    }
+}
---
+++ UserManagement/Repository/PersonRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserManagement.Context;
+using UserManagement.Models;
+using UserManagement.Repository.Interface;
+
+namespace UserManagement.Repository
+{
+    public class PersonRepository : IPersonRepository
+    {
+        private readonly MyContext conn;
+        public  PersonRepository(MyContext conn)
+        {
+            this.conn = conn;
+        }
+
+        //COBA2 DELETE SYNTAX
+        public int delete(string NIK)
+        {
+            var delPerson = conn.Persons.Find(NIK);
+            conn.Persons.Remove(delPerson);
+            var result = conn.SaveChanges();
+            return result;
+        }
+
+        public IEnumerable<Person> Get()
+        {
+            IEnumerable<Person> persons = new List<Person>();
+            persons = conn.Persons.ToList();
+            return persons;
+        }
+
+        public Person Get(string NIK)
+        {
+            var person = conn.Persons.Find(NIK);
+            return person;
+        }
+
+        public int insert(Person person)
+        {
+            conn.Persons.Add(person);
+            var result = conn.SaveChanges();
+            return result;
+        }
+
+        public int update(Person person)
+        {
+            conn.Persons.Update(person);
+            var result = conn.SaveChanges();
+            return result;
+        }
+    }
+}
